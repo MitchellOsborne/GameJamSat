26561bf92fea8ba136a4b99a135ef0ca1b03c39a
James Addison <jamesaddison93@gmail.com>
2016-11-12 18:23:12 +1100

    Added crack

--- Tank project/Assets/Game/Scripts/Health.cs
+++ Tank project/Assets/Game/Scripts/Health.cs
@@ -4,12 +4,19 @@ using System.Collections;
 public class Health : MonoBehaviour {
 
 	[SerializeField]
+	private GameObject hurtEffect;
+
+	[SerializeField]
 	private int maxHealth;
 
 	[SerializeField]
 	private int health;
 
 	public void Modify(int amount){
+		if (amount < 0 && hurtEffect != null) {
+			hurtEffect.SetActive (true);
+		}
+
 		health += amount;
 		health = Mathf.Clamp (health, 0, maxHealth);
 	}
