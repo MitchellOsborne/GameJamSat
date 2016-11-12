363957d791a81408b29b8bab30099718bdb8f229
James Addison <jamesaddison93@gmail.com>
2016-11-12 13:31:47 +1100

    PickUps

---
+++ Tank project/Assets/PickUp.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUp : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject turret;
+
+	[SerializeField]
+	private GameObject effect;
+
+	void OnTriggerEnter(Collider other) {
+		Instantiate (effect, transform.position, Quaternion.identity);
+
+		//TankController tc = other.gameObject.GetComponent<TankController>();
+
+		//tc.PickUp(turret);
+
+		Destroy(this);
+	}
+}
