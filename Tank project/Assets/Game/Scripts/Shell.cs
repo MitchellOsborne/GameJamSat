3934b3266597d686279c14f2980d2c8098a2d1ba
James Addison <jamesaddison93@gmail.com>
2016-11-12 12:51:02 +1100

    Missiles and health

---
+++ Tank project/Assets/Game/Scripts/Shell.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shell : MonoBehaviour {
+
+	[SerializeField]
+	private int damage;
+
+	[SerializeField]
+	private float speed;
+
+	[SerializeField]
+	private GameObject explosion;
+
+	private Rigidbody body;
+
+	// Use this for initialization
+	void Awake() {
+		body.velocity = transform.forward * speed;	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Instantiate (explosion, transform.position, Quaternion.identity);
+
+		Health health = other.gameObject.GetComponent<Health> ();
+		health.Modify (-damage);
+
+		Destroy(this);
+	}
+}
